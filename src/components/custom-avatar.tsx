5a512c56b0907742a2eabca2dfc580075d89eec4
AlexWang233 <alex13901350@gmail.com>
2024-01-21 18:46:45 -0500

    Avatar and GraphQL

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,24 @@
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
+
+type Props = AvatarProps & {
+  name: string;
+};
+
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
+  return (
+    <AntdAvatar
+      alt={"Alex Wang"}
+      size="small"
+      style={{
+        backgroundColor: `#87d068`,
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+      }}
+    >
+      AW
+    </AntdAvatar>
+  );
+};
+
+export default CustomAvatar;
--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,6 +1,11 @@
 import { Popover, Button } from "antd";
+import CustomAvatar from "../custom-avatar";
+import { useGetIdentity } from "@refinedev/core";
+import type { User } from "@/graphql/schema.types";
 
 const CurrentUser = () => {
+  const { data: user } = useGetIdentity<User>();
+
   return (
     <>
       <Popover
@@ -8,7 +13,9 @@ const CurrentUser = () => {
         trigger="click"
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
-      ></Popover>
+      >
+        <CustomAvatar />
+      </Popover>
     </>
   );
 };
