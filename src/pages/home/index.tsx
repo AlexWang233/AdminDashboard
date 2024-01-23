0a2e85b6d92f8445d94656ae4e5138ba5757dcd6
AlexWang233 <alex13901350@gmail.com>
2024-01-23 02:16:01 -0500

    Hompage init

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { Col, Row } from "antd";
+import React from "react";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
+        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+          CalendarUpcomingEvents
+        </Col>
+        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+          DashboardDealsChart
+        </Col>
+      </Row>
+    </div>
+  );
+};
