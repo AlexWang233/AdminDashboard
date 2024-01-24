4625f802011693ce899691a66c6b599a90c30bf1
AlexWang233 <alex13901350@gmail.com>
2024-01-24 00:25:15 -0500

    Homepage Proportion Fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,11 +5,22 @@ import React from "react";
 export const Home = () => {
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+      </Row>
       <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
         <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
           <UpcomingEvents />
         </Col>
-        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+        <Col xs={24} sm={24} xl={16} style={{ height: "460px" }}>
           <DealsChart />
         </Col>
       </Row>
