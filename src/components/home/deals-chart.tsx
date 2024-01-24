ecf89495d883dbe50ad07df92961c228d4f7c170
AlexWang233 <alex13901350@gmail.com>
2024-01-24 00:10:17 -0500

    Deals Chart Init

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,7 +1,22 @@
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
 import React from "react";
+import { Text } from "../text";
 
 const DealsChart = () => {
-  return <div>DealsChart</div>;
+  return (
+    <Card
+      style={{ height: "100%" }}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "24px 24px 0 24px" }}
+      title={
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.5rem" }}></Text>
+        </div>
+      }
+    ></Card>
+  );
 };
 
 export default DealsChart;
