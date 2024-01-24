c616d502433dd1bd1e5c6bff12f48052d352707b
AlexWang233 <alex13901350@gmail.com>
2024-01-24 00:17:23 -0500

    Chart Init

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -3,9 +3,28 @@ import { Card } from "antd";
 import React from "react";
 import { Text } from "../text";
 import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import { mapDealsData } from "@/utilities/helpers";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { DashboardDealsChartQuery } from "@/graphql/types";
 
 const DealsChart = () => {
-  const config: AreaConfig = { data: [] };
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: "dealStages",
+    meta: { gqlQuery: DASHBOARD_DEALS_CHART_QUERY },
+  });
+
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data]);
+
+  const config: AreaConfig = {
+    data: dealData,
+    xField: "timeText",
+    yField: "value",
+  };
+
   return (
     <Card
       style={{ height: "100%" }}
