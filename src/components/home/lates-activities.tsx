25de6a7e3c0579f01afec7305e944df7be88cc64
AlexWang233 <alex13901350@gmail.com>
2024-01-25 16:44:32 -0500

    Latest Activities Skeleton

--- src/components/home/lates-activities.tsx
+++ src/components/home/lates-activities.tsx
@@ -1,7 +1,35 @@
+import { UnorderedListOutlined } from "@ant-design/icons";
+import { Text } from "../text";
 import React from "react";
+import { Card, List } from "antd";
+import LatestActivitiesSkeleton from "../skeleton/latest-activities";
 
 const LatestActivities = () => {
-  return <div>LatestActivities</div>;
+  const isLoading = false;
+  return (
+    <Card
+      headStyle={{ padding: "16px" }}
+      bodyStyle={{ padding: "0 1rem" }}
+      title={
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <UnorderedListOutlined />{" "}
+          <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+            Latest Activities
+          </Text>
+        </div>
+      }
+    >
+      {isLoading ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={Array.from({ length: 5 }).map((_, i) => ({ id: i }))}
+          renderItem={(_, index) => <LatestActivitiesSkeleton key={index} />}
+        />
+      ) : (
+        <List />
+      )}
+    </Card>
+  );
 };
 
 export default LatestActivities;
