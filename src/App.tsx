be15511b9d5a33dad6c5f4c4b4f99b50c1ffbc36
AlexWang233 <alex13901350@gmail.com>
2024-01-27 22:11:59 -0500

    Create Company Page Init

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import { ForgotPassword, Home, Login, Register } from "./pages";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import { CompanyList } from "./pages/company/list";
+import { Create } from "./pages/company/create";
 
 function App() {
   return (
@@ -62,7 +63,10 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
-                  <Route path="/companies" element={<CompanyList />} />
+                  <Route path="/companies">
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<Create />} />
+                  </Route>
                 </Route>
               </Routes>
               <RefineKbar />
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export * from "./home";
 export * from "./login";
 export * from "./register";
 export * from "./company/list";
+export * from "./company/create";
