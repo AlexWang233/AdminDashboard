56336800eddf3b0d728b58579b963b845cc4207a
AlexWang233 <alex13901350@gmail.com>
2024-01-29 01:44:59 -0500

    Edit Companies Layout Init

--- src/pages/company/edit.tsx
+++ src/pages/company/edit.tsx
@@ -1,5 +1,38 @@
+import CustomAvatar from "@/components/custom-avatar";
+import { UPDATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import { getNameInitials } from "@/utilities";
+import { Edit, useForm } from "@refinedev/antd";
+import { Col, Form, Row } from "antd";
 import React from "react";
 
-export const Edit = () => {
-  return <div>Edit</div>;
+export const EditPage = () => {
+  const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
+    redirect: false,
+    meta: {
+      gqlMutation: UPDATE_COMPANY_MUTATION,
+    },
+  });
+  const { avatarUrl, name } = queryResult?.data?.data || {};
+  return (
+    <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} xl={12}>
+          <Edit
+            isLoading={formLoading}
+            saveButtonProps={saveButtonProps}
+            breadcrumb={false}
+          >
+            <Form {...formProps} layout="vertical">
+              <CustomAvatar
+                shape="square"
+                src={avatarUrl}
+                name={getNameInitials(name || "")}
+                style={{ width: 96, height: 96, marginBottom: "24px" }}
+              />
+            </Form>
+          </Edit>
+        </Col>
+      </Row>
+    </div>
+  );
 };
