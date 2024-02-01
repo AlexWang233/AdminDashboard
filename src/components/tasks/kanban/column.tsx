946194fb3270b39dc77f299a4b74ec824c2a7808
AlexWang233 <alex13901350@gmail.com>
2024-02-01 03:33:35 -0500

    Kanban Init

--- src/components/tasks/kanban/column.tsx
+++ src/components/tasks/kanban/column.tsx
@@ -1,4 +1,7 @@
+import { Text } from "@/components/text";
+import { PlusOutlined } from "@ant-design/icons";
 import { useDroppable } from "@dnd-kit/core";
+import { Badge, Button, Space } from "antd";
 import React from "react";
 
 const KanbanColumn = () => {
@@ -6,7 +9,38 @@ const KanbanColumn = () => {
     id: "",
     data: "",
   });
-  return <div ref={setNodeRef} style={{ display: flex }}></div>;
+  const count = 3;
+  const description = "Description";
+  const title = "Title";
+  const onAddClickHandler = () => {};
+  return (
+    <div
+      ref={setNodeRef}
+      style={{ display: "flex", flexDirection: "column", padding: "0 16px" }}
+    >
+      <div style={{ padding: "12px" }}>
+        <Space style={{ width: "100%", justifyContent: "space-between" }}>
+          <Space>
+            <Text
+              ellipsis={{ tooltip: title }}
+              size="xs"
+              strong
+              style={{ textTransform: "uppercase", whiteSpace: "nowrap" }}
+            >
+              {title}
+            </Text>
+            {!!count && <Badge count={count} color="cyan" />}
+          </Space>
+          <Button
+            shape="circle"
+            icon={<PlusOutlined />}
+            onClick={onAddClickHandler}
+          />
+        </Space>
+        {description}
+      </div>
+    </div>
+  );
 };
 
 export default KanbanColumn;
