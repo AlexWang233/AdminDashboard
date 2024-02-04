623570fa12733ef0ec326abb483a7499cd4d385e
AlexWang233 <alex13901350@gmail.com>
2024-02-04 14:54:42 -0500

    Kanban Card

--- src/components/tasks/kanban/card.tsx
+++ src/components/tasks/kanban/card.tsx
@@ -1,5 +1,8 @@
 import { User } from "@/graphql/schema.types";
-import React from "react";
+import { Button, Card, ConfigProvider, Dropdown, MenuProps, theme } from "antd";
+import { Text } from "@/components/text";
+import React, { useMemo } from "react";
+import { EyeOutlined, MoreOutlined } from "@ant-design/icons";
 
 type ProjectCardProps = {
   id: string;
@@ -14,7 +17,64 @@ type ProjectCardProps = {
 };
 
 const ProjectCard = ({ id, title, dueDate, users }: ProjectCardProps) => {
-  return <div>ProjectCard</div>;
+  const { token } = theme.useToken();
+  const edit = () => {};
+  const dropdownItems = useMemo(() => {
+    const dropdownItems: MenuProps["items"] = [
+      {
+        label: "View Card",
+        key: "1",
+        icon: <EyeOutlined />,
+        onClick: () => {
+          edit();
+        },
+      },
+      {
+        danger: true,
+        label: "Delete Card",
+        key: "2",
+        onClick: () => {},
+      },
+    ];
+    return dropdownItems;
+  }, []);
+  return (
+    <div>
+      <ConfigProvider
+        theme={{
+          components: {
+            Tag: {
+              colorText: token.colorTextSecondary,
+            },
+            Card: {
+              headerBg: "transparent",
+            },
+          },
+        }}
+      >
+        <Card
+          size="small"
+          title={<Text ellipsis={{ tooltip: title }}>{title}</Text>}
+          onClick={() => edit()}
+          extra={
+            <Dropdown trigger={["click"]} menu={{ items: dropdownItems }}>
+              <Button
+                type="text"
+                shape="circle"
+                icon={<MoreOutlined style={{ transform: "rotate(90deg)" }} />}
+                onPointerDown={(e) => {
+                  e.stopPropagation();
+                }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              />
+            </Dropdown>
+          }
+        ></Card>
+      </ConfigProvider>
+    </div>
+  );
 };
 
 export default ProjectCard;
